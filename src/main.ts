fd19f1093dda48d7c6daee4c41a5ecbb18562508
Kamil Pawlaczyk <prgmr.kamil.pawlaczyk@gmail.com>
2022-01-29 15:03:33 +0100

    feat: add env variables

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,18 @@ import express, { Application, RequestHandler } from 'express'
 import { Controller, Server } from './typings'
 import { FilesController } from './files/files.controller'
 import { FilesService } from './files/files.service'
+import config from './config'
+
+config()
 
 const app: Application = express()
 
 const controllers: Controller[] = [new FilesController(new FilesService())]
 const middlewares: RequestHandler[] = [bodyParser.json()]
 
-const server = new Server(app, 8080)
+const port: number = process.env.PORT ? parseInt(process.env.PORT) : 8080
+
+const server = new Server(app, port)
 
 server.loadControllers(controllers)
 server.loadMiddlewares(middlewares)
