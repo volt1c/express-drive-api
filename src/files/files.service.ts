5d220c50f7eef11de10fd6ded6b63e7a63b4a5b8
Kamil Pawlaczyk <prgmr.kamil.pawlaczyk@gmail.com>
2022-01-31 23:41:12 +0100

    feat(files.service): add unimplemented methods

--- src/files/files.service.ts
+++ src/files/files.service.ts
@@ -1,3 +1,16 @@
+import { UploadedFile } from 'express-fileupload'
 import { Service } from '../typings'
 
-export class FilesService extends Service {}
+export class FilesService extends Service {
+  upload(userId: string, file: UploadedFile): string {
+    throw Error('Not implemented')
+  }
+
+  getFilePath(userId: string, fileName: string): string {
+    throw Error('Not implemented')
+  }
+
+  getFilesNames(userId: string, path = ''): string[] {
+    throw Error('Not implemented')
+  }
+}
